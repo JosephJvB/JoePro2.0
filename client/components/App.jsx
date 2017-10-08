d32ab0999f9828a14e39dacd716a8145f880cedd
Richard Johns <richard.johns193@gmail.com>
2017-10-08 16:32:03 +1300

    popped in some basic bulma

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 
+import Header from './Header'
 import Map from './Map'
 // import Info from './Info'
 
 const App = () => {
   return (
-    <div>
-      <h1>This Joe App :)</h1>
-      <Map />
-    </div>
+    <section className="section has-text-centered">
+      <Header />
+      <hr />
+      <div className="columns">
+        <div className="column">
+          <Map />
+        </div>
+        <div className="column">
+          <h1>info</h1>
+        </div>
+      </div>
+    </section>
   )
 }
 
