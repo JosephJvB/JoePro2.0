9c7f75cd9e45dc3191b630637220aca79ebb2389
Richard Johns <richard.johns193@gmail.com>
2017-10-14 17:13:28 +1300

    didnt go through with likes im gonna make a route to a spotpage

--- client/components/Info.jsx
+++ client/components/Info.jsx
@@ -1,31 +1,47 @@
 import React from 'react'
 
-const Info = (props) => {
-  return (
-    <div className="box is-info">
-      <div className="columns">
-        <div className="column is-1">
+class Info extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      likes: props.activeSpot.likes
+    }
+    // binds go here
+    this.handleLike = this.handleLike.bind(this)
+  }
+
+  handleLike () {
+    this.setState({
+    })
+  }
+
+  render () {
+    return (
+      <div className="box is-info">
+        <div className="columns">
+          <div className="column is-1">
+          </div>
+          <div className="column has-text-left">
+            <h3 className="subtitle is-3">{this.props.activeSpot.spotName}</h3>
+            <hr />
+          </div>
+          <div className="column is-2">
+            <button className="button is-success is-pulled-left" onClick={() => this.handleLike()}>Like</button>
+          </div>
         </div>
-        <div className="column has-text-left">
-          <h3 className="subtitle is-3">{props.activeSpot.spotName}</h3>
-          <hr />
-        </div>
-        <div className="column is-2">
-          <button className="button is-success is-pulled-left">Like</button>
+        <div className="columns has-text-left">
+          <div className="column is-1">
+          </div>
+          <div className="column">
+            <h3 className="subtitle is-4"><strong>Address:</strong> {this.props.activeSpot.address}</h3>
+            <h3 className="subtitle is-4"><strong>Terrain:</strong> {this.props.activeSpot.terrain}</h3>
+            <h3 className="subtitle is-4"><strong>Comments:</strong> {this.props.activeSpot.comments}</h3>
+            <h3 className="subtitle is-4"><strong>Likes:</strong> {this.props.activeSpot.likes}</h3>
+          </div>
         </div>
       </div>
-      <div className="columns has-text-left">
-        <div className="column is-1">
-        </div>
-        <div className="column">
-          <h3 className="subtitle is-4">Address: {props.activeSpot.address}</h3>
-          <h3 className="subtitle is-4">Terrain: {props.activeSpot.terrain}</h3>
-          <h3 className="subtitle is-4">Comments: {props.activeSpot.comments}</h3>
-          <h3 className="subtitle is-4">Likes: {props.activeSpot.likes}</h3>
-        </div>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Info
---
+++ client/components/SingleSpot.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SingleSpot = () => {
+  return (
+    <div>
+  im a single spot
+    </div>
+  )
+}
+
+export default SingleSpot
