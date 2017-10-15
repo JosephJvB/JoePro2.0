a0b557b8845bb312a771890ce5775f3e45ad5e5f
Richard Johns <richard.johns193@gmail.com>
2017-10-15 18:11:25 +1300

    i have a very basic route setup but hotdog its working

--- client/components/SingleSpot.jsx
+++ client/components/SingleSpot.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+// import {match} from 'react-router-dom'
 
-import Header from './Header'
-
+// const SingleSpot = ({match}) => {
 const SingleSpot = (props) => {
   return (
     <div>
-      <Header />
-      wanna render pictures and gifs and info idk, check in button and active skaters
+      {/* you are at {match.params.spotName} */}
+      <h1>{props.activeSpot.spotName}</h1>
+     want to show pics and gifs and stats and things and stuff and wow look at this website! *applause*
+      <a href='/'><button className='button is-danger'>snap back to reality</button></a>
     </div>
   )
 }
