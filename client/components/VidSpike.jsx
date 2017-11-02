1ecb5b78fcc812a4a5e7709ee2d28ee247a64662
dazyang <dazyang.contact@gmail.com>
2017-11-02 14:59:12 +1300

    got the sequence to work

--- client/components/VidSpike.jsx
+++ client/components/VidSpike.jsx
@@ -4,21 +4,51 @@ import YouTube from 'react-youtube'
 class VidSpike extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    this.state = {
+      video: null,
+      startTime: 0,
+      quoteStart: 6,
+      quoteEnd: 9,
+      pauseTime: 5
+    }
     // binds go here
-    this.onPauseVideo = this.onPauseVideo.bind(this)
-    this.onReadyVideo = this.onReadyVideo.bind(this)
+    this.startClip = this.startClip.bind(this)
+    this.muteClip = this.muteClip.bind(this)
+    this.pauseClip = this.pauseClip.bind(this)
+    this.restartClip = this.restartClip.bind(this)
+    this.endVideo = this.endVideo.bind(this)
   }
   // handlers here
-
-  onReadyVideo (event) {
-    console.log('playing the video')
+  startClip (event) {
+    console.log(event)
+    console.log('vid playing')
+    this.setState({
+      video: event.target
+    })
     event.target.playVideo()
+    setTimeout(() => this.muteClip(), this.state.quoteStart * 1000)
   }
 
-  onPauseVideo (event) {
-    console.log('you have 5 seconds to finish the quote!')
-    setTimeout(() => event.target.playVideo(), 5000)
+  muteClip () {
+    console.log('be quiet')
+    this.state.video.mute()
+    setTimeout(() => this.pauseClip(), this.state.quoteEnd * 1000)
+  }
+  pauseClip () {
+    console.log('let them speak')
+    this.state.video.pauseVideo()
+    setTimeout(() => this.restartClip(), this.state.pauseTime * 1000)
+  }
+  restartClip () {
+    console.log('do-over!')
+    this.state.video.seekTo(this.state.startTime)
+    this.state.video.unMute()
+    setTimeout(() => this.endVideo(), 1000)
+  }
+  endVideo () {
+    console.log('oooh thats what you said')
+    this.state.video.playVideo()
+    setTimeout(() => this.state.video.pauseVideo(), this.state.quoteEnd * 1000)
   }
 
   render () {
@@ -27,14 +57,18 @@ class VidSpike extends React.Component {
       width: '640'
     }
     return (
-      <div>hi im a video
+      <div>
 
-        <YouTube videoId="3j3_iPskjxk" opts={opts} onPause={this.onPauseVideo} onReady={this.onReadyVideo}/>
+        <YouTube videoId="VQNoPqLuZ74" opts={opts} onReady={this.startClip}/>
+
+        <button>probably need a continue button, visible after, to go to leaderboard or next player</button>
       </div>
     )
   }
 }
 
 export default VidSpike
+
+// pseudocode time, I want the video to play for ~30 seconds, I want the video to pause for ~15 seconds for the player to finish the quote. I want the video to resume and finish the quote
 
 // YouTube.PlayerState
